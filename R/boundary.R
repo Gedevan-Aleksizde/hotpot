de75fa9d25488ee09fa6fe3d5cf949523b19ea10
Satoshi Katagiri <katagiri.stsh@gmail.com>
2024-04-04 21:14:35 +0900

    circle function's wrong radius fixed

--- R/boundary.R
+++ R/boundary.R
@@ -1,5 +1,5 @@
 #' @export
-boundary_half_circle <- function(x, .r = sqrt(2), a = 0, b = 0){
+boundary_half_circle <- function(x, .r = sqrt(2)/2, a = 0, b = 0){
   return(ifelse(x < 0, sqrt(.r^2 - (x + .r - a)^2) + b, -sqrt(.r^2 - (x - .r - a)^2 + b)))
 }
 
